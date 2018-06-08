794af31ee75dc22a0da5b60a9f2f12ae7a04c17b
gaoshan <sgao@daicy.net>
2018-06-08 16:13:41 +0800

    set font

--- init.el
+++ init.el
@@ -7,3 +7,16 @@
 (require 'init-modes)
 (require 'init-hotkeys)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 144 :width normal)))))
